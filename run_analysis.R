6ed4f07f822fc582a0d0ef8bc27f67462770ce45
Bookwrm911 <Bookwrm911@users.noreply.github.com>
2016-05-29 01:21:12 -0400

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,57 +1,52 @@
-#Download File
+# Make sure Have Necessary Package active
+library(reshape2)
+
+#Download file
 
 if(!file.exists("./data")){dir.create("./data")}
 fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
 download.file(fileUrl,destfile="./data/Dataset.zip")
-##If running on a Mac, may need to add " method = "curl" to above code to work. U
+##If running on a Mac, may need to add " method = "curl" to above code to work. 
 
 #Unzip File
 
 unzip(zipfile="./data/Dataset.zip",exdir="./data")
 path_rf <- file.path("./data" , "UCI HAR Dataset")
 
-#Read Relavant Data
+# Load activity labels + features
+ActLabels <- read.table("UCI HAR Dataset/activity_labels.txt")
+ActLabels[,2] <- as.character(ActLabels[,2])
+Features <- read.table("UCI HAR Dataset/features.txt")
+Features[,2] <- as.character(Features[,2])
 
-SubTest <- read.table(file.path(path_rf, "train", "subject_train.txt"),header = FALSE)
-SubTrain <- read.table(file.path(path_rf, "test" , "subject_test.txt"),header = FALSE)
-ActTest  <- read.table(file.path(path_rf, "test" , "Y_test.txt" ),header = FALSE)
-ActTrain <- read.table(file.path(path_rf, "train", "Y_train.txt"),header = FALSE)
-FeatTest  <- read.table(file.path(path_rf, "test" , "X_test.txt" ),header = FALSE)
-FeatTrain <- read.table(file.path(path_rf, "train", "X_train.txt"),header = FALSE)
+# Extract only the data on mean and standard deviation
+M.STD <- grep(".*mean.*|.*std.*", Features[,2])
+M.STD.names <- features[M.STD,2]
+M.STD.names = gsub('-mean', 'Mean', M.STD.names)
+M.STD.names = gsub('-std', 'Std', M.STD.names)
+M.STD.names <- gsub('[-()]', '', M.STD.names)
 
-##Merge Data sets
 
-Sub <- rbind(SubTrain, SubTest)
-Act<- rbind(ActTrain, ActTest)
-Feat<- rbind(FeatTrain, FeatTest)
+# Load the datasets
+Train <- read.table("UCI HAR Dataset/train/X_train.txt")[M.STD]
+TrainAct <- read.table("UCI HAR Dataset/train/Y_train.txt")
+TrainSub <- read.table("UCI HAR Dataset/train/subject_train.txt")
+Train <- cbind(TrainSub, TrainAct, Train)
 
-names(Sub)<-c("subject")
-names(Act)<- c("activity")
-FeatNames <- read.table(file.path(path_rf, "features.txt"),head=FALSE)
-names(Feat)<- FeatNames$V2
+Test <- read.table("UCI HAR Dataset/test/X_test.txt")[M.STD]
+TestAct <- read.table("UCI HAR Dataset/test/Y_test.txt")
+TestSub <- read.table("UCI HAR Dataset/test/subject_test.txt")
+Test <- cbind(TestSub, TestAct, Test)
 
-Combine <- cbind(Sub, Act)
-Data <- cbind(Feat, Combine)
+# Merge datasets and add labels
+allData <- rbind(Train, Test)
+colnames(allData) <- c("subject", "activity", M.STD.names)
 
-#Find Means & Standard Deviations
+# turn activities & subjects into factors
+allData$activity <- factor(allData$activity, levels = ActLabels[,1], labels = ActLabels[,2])
+allData$subject <- as.factor(allData$subject)
 
-FeatNameSubset<-FeatNames$V2[grep("mean\\(\\)|std\\(\\)", FeatNames$V2)]
+allData.melted <- melt(allData, id = c("subject", "activity"))
+allData.mean <- dcast(allData.melted, subject + activity ~ variable, mean)
 
-selected<-c(as.character(FeatNameSubset), "subject", "activity" )
-Data<-subset(Data,select=selected)
-
-#Uses descriptive activity names to name the activities in the data set
-
-names(Data)<-gsub("^t", "Time", names(Data))
-names(Data)<-gsub("^f", "Frequency", names(Data))
-names(Data)<-gsub("Acc", "Accelerometer", names(Data))
-names(Data)<-gsub("Gyro", "Gyroscope", names(Data))
-names(Data)<-gsub("Mag", "Magnitude", names(Data))
-names(Data)<-gsub("BodyBody", "Body", names(Data))
-
-#Create New Tidy Data Set
-
-library(dplyr);
-Data2<-aggregate(. ~subject + activity, Data, mean)
-Data2<-Data2[order(Data2$subject,Data2$activity),]
-write.table(Data2, file = "tidy_data.txt",row.name=FALSE)
+write.table(allData.mean, "tidy_data.txt", row.names = FALSE, quote = FALSE)
